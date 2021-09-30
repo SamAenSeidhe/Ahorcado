72a1d8856d6006afceccd7bbabdbaa31ef550399
SamAenSeidhe <64458012+SamAenSeidhe@users.noreply.github.com>
2021-09-30 18:56:23 -0500

    Update newmain.cpp
    main code

--- newmain.cpp
+++ newmain.cpp
@@ -1,4 +1,4 @@
-//Declaración de librerías para usar con el ahorco
+//DeclaraciÃ³n de librerÃ­as para usar con el ahorco
 #include <iostream>
 #include <cctype>
 #include <conio.h>
@@ -7,7 +7,7 @@
 #include <fstream>
 #include <ctime>
 
-//Declaración de espacios de nombres
+//DeclaraciÃ³n de espacios de nombres
 using namespace std;
 
 //Estructura de variables para el ahorcado
@@ -19,10 +19,10 @@ struct libreria{
 	int nivel = 1; 
 };
 
-//Declaración de llamada a estructuras
+//DeclaraciÃ³n de llamada a estructuras
 libreria banco;
 
-//Declaración de funciones para el ahorcado
+//DeclaraciÃ³n de funciones para el ahorcado
 void filtroPalabra(int caracterCadena);
 void palabraAleatoria();
 void tecladoES_LA();
@@ -34,7 +34,7 @@ void proximoNivel();
 void finJuego();
 int victoriaNivel();
 
-//Función principal de ejecución del código
+//FunciÃ³n principal de ejecuciÃ³n del cÃ³digo
 int main() {
 	
 	srand(time(NULL));
@@ -59,14 +59,14 @@ int main() {
 	finJuego();
 }
 
-//Función para configurar para localizar a Español Latinoamerica
+//FunciÃ³n para configurar para localizar a EspaÃ±ol Latinoamerica 
 void tecladoES_LA() {
 	setlocale(LC_ALL, "spanish");	
 	SetConsoleCP(1252);
 	SetConsoleOutputCP(1252);
 }
 
-//Función para obtener una palabra aleatoria
+//FunciÃ³n para obtener una palabra aleatoria
 void palabraAleatoria() {
 	string recorridoPalabras;	
 	int valorAleatorio = 1 + rand() % 1634;	
@@ -92,27 +92,27 @@ void longitudLineas() {
 
 void filtroPalabra(int caracterCadena) {
 	switch(banco.palabra[caracterCadena]) {
-		case 'á':
+		case 'Ã¡':
 			if(banco.letra[0] == 'a') {
 				banco.lineasPalabra[caracterCadena] = banco.palabra[caracterCadena];
 			}
 			break;
-		case 'é':
+		case 'Ã©':
 			if(banco.letra[0] == 'e') {
 				banco.lineasPalabra[caracterCadena] = banco.palabra[caracterCadena];
 			}
 			break;
-		case 'í':
+		case 'Ã­':
 			if(banco.letra[0] == 'i') {
 				banco.lineasPalabra[caracterCadena] = banco.palabra[caracterCadena];
 			}
 			break;
-		case 'ó':
+		case 'Ã³':
 			if(banco.letra[0] == 'o') {
 				banco.lineasPalabra[caracterCadena] = banco.palabra[caracterCadena];
 			}
 			break;
-		case 'ú':
+		case 'Ãº':
 			if(banco.letra[0] == 'u') {
 				banco.lineasPalabra[caracterCadena] = banco.palabra[caracterCadena];
 			}
@@ -120,15 +120,15 @@ void filtroPalabra(int caracterCadena) {
 	}
 }
 
-//Función para verificar el caracter ingresado como letra
+//FunciÃ³n para verificar el caracter ingresado como letra
 void verificarLetra() {
 	while(!isalpha(banco.letra[0])) {
-		cout<<endl<<"No se permiten caracteres que no sean alfabéticos."<<endl;
+		cout<<endl<<"No se permiten caracteres que no sean alfabÃ©ticos."<<endl;
 		banco.letra = getch();
 	}
 }
 
-//Función para verificar si la letra está en la palabra
+//FunciÃ³n para verificar si la letra estÃ¡ en la palabra
 void contarLetra() {
 	verificarLetra();
 	banco.estadoAnterior = banco.lineasPalabra;
@@ -146,14 +146,14 @@ void contarLetra() {
 	cout<<endl<<banco.lineasPalabra<<endl;
 }
 
-//Función para verificar el conteo de intentos
+//FunciÃ³n para verificar el conteo de intentos
 void conteoIntentos() {
 	if(banco.estadoAnterior == banco.lineasPalabra) {
 		banco.intento--;
 	}
 }
 
-//Función para verificar si se ha logrado adivinar la palabra
+//FunciÃ³n para verificar si se ha logrado adivinar la palabra
 int victoriaNivel() {
 	if(banco.palabra == banco.lineasPalabra) {
 		return 1;
@@ -164,13 +164,13 @@ int victoriaNivel() {
 	}
 } 
 
-//Función para avanzar nivel de dificultad
+//FunciÃ³n para avanzar nivel de dificultad
 void proximoNivel() {
 	banco.nivel++;
 	palabraAleatoria();
 }
 
-//Función para indicar la finalización del juego (Derrota o victoria)
+//FunciÃ³n para indicar la finalizaciÃ³n del juego (Derrota o victoria)
 void finJuego() {
 	banco.estatusJuego = (banco.nivel == 7) ? "Has ganado el juego" : "Has perdido el juego";
 	cout<<banco.estatusJuego<<endl;
